dc6b5777ecb27e3b3ca8aa4e71680cfc44f0dda8
savagenur <savagenurkg@gmail.com>
2021-09-16 15:04:35 +0600

    Assign agents

--- leads/views.py
+++ leads/views.py
@@ -4,7 +4,7 @@ from agents.mixins import OrganisorAndLoginRequiredMixin
 from django.http import HttpResponse
 from django.views import generic
 from .models import Lead, Agent
-from .forms import LeadForm, LeadModelForm, CustomUserCreationForm
+from .forms import LeadForm, LeadModelForm, CustomUserCreationForm, AssignAgentForm
 
 
 class SignupView(generic.CreateView):
@@ -29,13 +29,25 @@ class LeadListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         if self.request.user.is_organisor:
-            queryset = Lead.objects.filter(organisation=self.request.user.userprofile)
+            queryset = Lead.objects.filter(
+                organisation=self.request.user.userprofile,
+                agent__isnull=False)
         else:
             queryset = Lead.objects.filter(organisation=self.request.user.agent.organisation)
             queryset = queryset.filter(agent__user=self.request.user)
 
         return queryset
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(LeadListView, self).get_context_data(**kwargs)
+        if self.request.user.is_organisor:
+            queryset = Lead.objects.filter(
+                organisation=self.request.user.userprofile,
+                agent__isnull=True)
+            context.update({
+                "unassigned_leads": queryset
+            })
+        return context
 
 # def lead_list(request):
 #     leads = Lead.objects.all()
@@ -124,6 +136,50 @@ class LeadDeleteView(OrganisorAndLoginRequiredMixin, generic.DeleteView):
 
     def get_success_url(self):
         return reverse('leads:lead-list')
+
+
+class AssignAgentView(OrganisorAndLoginRequiredMixin, generic.FormView):
+    template_name = 'leads/assign_agent.html'
+    form_class = AssignAgentForm
+
+    def get_form_kwargs(self, **kwargs):
+        kwargs = super(AssignAgentView, self).get_form_kwargs(**kwargs)
+        kwargs.update({
+            'request': self.request
+        })
+        return kwargs
+
+    def get_success_url(self):
+        return reverse('leads:lead-list')
+
+    def form_valid(self, form):
+        agent = form.cleaned_data['agent']
+        lead = Lead.objects.get(id=self.kwargs['pk'])
+        lead.agent = agent
+        lead.save()
+        return super(AssignAgentView, self).form_valid(form)
+
+
+# class AssignAgentV(OrganisorAndLoginRequiredMixin, generic.FormView):
+#     template_name = 'leads/assign_agent.html'
+#     form_class = AssignAgentF
+#
+#     def get_form_kwargs(self, **kwargs):
+#         kwargs = super(AssignAgentV, self).get_form_kwargs(**kwargs)
+#         kwargs.update({
+#             'request': self.request
+#         })
+#         return kwargs
+#
+#     def get_success_url(self):
+#         return reverse('leads:lead-list')
+#
+#     def form_valid(self, form):
+#         agent = form.cleaned_data['agent']
+#         lead = Lead.objects.get(id=self.kwargs['pk'])
+#         lead.agent = agent
+#         lead.save()
+#         return super(AssignAgentV, self).form_valid(form)
 
 
 # def lead_delete(request, pk):
